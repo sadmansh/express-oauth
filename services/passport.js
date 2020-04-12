49cf5143a72c46184b3cf668dab13310ea89cbcc
Sadman Shawmik <hello@sadmansh.com>
2020-04-13 01:45:07 +0600

    Refactor promise using async/await

--- services/passport.js
+++ services/passport.js
@@ -24,10 +24,8 @@ passport.use(new GoogleStrategy(
 	}, 
 	async (accessToken, refreshToken, profile, done) => {
 		const existingUser = await User.findOne({ googleId: profile.id })
-		if (existingUser) done(null, existingUser)
-		else {
-			const user = await new User({ googleId: profile.id }).save()
-			done(null, user)
-		}
+		if (existingUser) return done(null, existingUser)
+		const user = await new User({ googleId: profile.id }).save()
+		done(null, user)
 	}
 ))
