fbd5c69423cbebf42779f3d82f6cda7f58d9c5d0
Sadman Shawmik <hello@sadmansh.com>
2020-04-15 06:29:54 +0600

    Redirect to dashboard on login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
 		})
 	)
 
-	app.get('/auth/google/callback', passport.authenticate('google'))
+	app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => res.redirect('/dashboard'))
 
 	app.get('/api/current_user', (req, res) => {
 		res.send(req.user)
